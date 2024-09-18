8e3a118755cd7b2bc7c5bd85a47ea8f00e1a2bda
Chirri7 <varelaj565@gmail.com>
2024-09-18 15:13:52 -0500

    Add files via upload

---
+++ 2. Variables.py
@@ -0,0 +1,7 @@
+#Espacios en memoria para almacenar datos, Existen varios tipos de datos como cadenas, numeros enteros, numeros flotantes. 
+#ponemos lo siguiente nombre = cotenido de la variable
+Nombre1 = "Juan";
+Nombre2 = "David";
+print(Nombre1);
+print(Nombre2);
+#para nombrar variables tenemos que tener cuidado con las palabras reservadas como class, estas palabras no se pueden usar. 
