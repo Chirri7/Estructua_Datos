798921f0c142c4e102459deba22ff28806569cfc
Chirri7 <varelaj565@gmail.com>
2024-09-18 15:41:54 -0500

    Add files via upload

---
+++ 4. Variables_Salida.py
@@ -0,0 +1,10 @@
+#   Print puede mostrar multiples variables llamadas comas. 
+j = "juan"
+d = "david"
+v = "varela"
+print(j,d,v)
+#Podemos hacer sumas con el simbolo +
+x = 10 
+y = 30
+print(x + y) # no podemos sumar una cadena con un entero, phyton nos dara error 
+print(x,j)#De esta manera podremos mostrar una cadena y un numero de manera correcta 
