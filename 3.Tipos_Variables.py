7f94909076ce61897abf224b1b362b672ab9167a
Chirri7 <varelaj565@gmail.com>
2024-09-18 15:29:42 -0500

    Add files via upload

---
+++ 3.Tipos_Variables.py
@@ -0,0 +1,13 @@
+#Existen varios tipos de variables, podemos usar la funcion type para que la consola nos diga que tipo de variable es
+nombre = "David"
+nombre2 = "varela"
+print(type(nombre)) #Ese cosigo nos dice que tipo de variable es, nada mas eso. 
+
+#Tambien encontramos la indexacion, esta funciona para tomar una sola letra del String 
+print(nombre[0]) #Aqui estoy tomando la primera letra 
+print(nombre[-1]) #Aqui estoy tomando la ultima letra 
+
+print(nombre + " " + nombre2) #Aqui lo que hago es unir el valor de dos variables 
+print(nombre2 * 4) #Aqui lo que estoy haciendo es que se repita 4 veces el resultado por consola 
+
+print(len(nombre2)) #Aqui me dice cuanto espacio tiene esta variables, me dice en otras pabras su logitud 
