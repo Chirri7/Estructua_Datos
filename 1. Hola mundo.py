d37b582af7341460b4b66d9c669cec9725a9b5e7
Chirri7 <varelaj565@gmail.com>
2024-09-18 15:04:09 -0500

    Add files via upload

---
+++ 1. Hola mundo.py
@@ -0,0 +1,7 @@
+#Por medio de los comentarios podremos explicar nuestro codigo
+#A continuacion se vera un hola mundo 
+print("Hola mundo "); 
+print('Mi primer print')
+#Por medio del print tambien podemos hacer operaciones como sumas, a continuacion aparece un ejemplo 
+print(20 + 10)
+
