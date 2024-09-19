395c025180068b4d629e7748f87e2c8ba9702e44
Chirri7 <varelaj565@gmail.com>
2024-09-19 15:35:38 -0500

    Add files via upload

---
+++ 11.Phyton Segmentacion de cadenas.py
@@ -0,0 +1,7 @@
+#si nosotros tenemos una frase tambien podremos tomar solo una pequeña parte de esa frases si queremos por lo siguiente
+b = "Yo estudio en la UIS"
+print(b[3:6]) #aqui tomamos el valor de est
+#si para el comando anterior quitamos el rango final, ira desde el numero del inciio hasta el final 
+print(b[3:]) #aqui tomamos el valor de est
+#tambien si ponemos los limites con valores negativos, tomaremos desde el final de la cadena
+print(b[-1:-2]) #aqui tomamos el valor de est
