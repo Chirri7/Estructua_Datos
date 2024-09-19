44f79adb463c91981d471473dade4d4a17063770
Chirri7 <varelaj565@gmail.com>
2024-09-19 14:22:54 -0500

    Add files via upload

---
+++ 6. Tipos de datos en phyton.py
@@ -0,0 +1,28 @@
+#Las variables van a almacernar diferentes tipos de datos, estos pueden ser enteros, cadenas, numeros flotantes etc... un comando 
+#para poder saber que tipo de dato es es el siguiente: 
+x = 77
+print(type(x))
+#En python, el tipo de dato se establece automaticamente dependiendo el contenido que se le quiera asignar. 
+q = "Sistemas lo mejor" #Aqui tendremos un String
+w = 77; #aqui tenemos un numero entero
+e = 77,98; #aqui se almacenara un numero flotante. 
+r = 1j; #Un valor de tipo complex
+t = ["tenis", "baloncesto", "futbol"] #se Almacena una lista 
+y = True # se asigna un Valor booleano 
+u = b"Hello" # bytes
+i = bytearray(5) #bytearray
+o = memoryview(bytes(5))#memoryview 
+p = None #Nonetype
+#Se imprimira los valores de cada una de las variables 
+print(x)
+print(q)
+print(w)
+print(e)
+print(r)
+print(t)
+print(y)
+print(u)
+print(i)
+print(o)
+print(p)
+
