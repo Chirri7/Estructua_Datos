15daa7cf7933b3f4df678dcc103ae4937ecb5a9d
Chirri7 <varelaj565@gmail.com>
2024-09-19 14:32:46 -0500

    Add files via upload

---
+++ 7. Numeros en Phyton.py
@@ -0,0 +1,30 @@
+#Los numeros en phyton son, int, float, complex 
+x = 7 #int
+y = 7,8 # float 
+d = 5j # complex
+#para verificar utilicemos la funcion type
+print(type(x))
+print(type(y))
+print(type(d))
+#Recordenmos que int es un numero entero postivo o negativo 
+c = 1
+v = 35656222554887711
+b = -3255522
+#comprobemos 
+print(type(c))
+print(type(v))
+print(type(b))
+#float Es un numero positivo o negrativo pero con numeros decimales 
+a = 4.10
+s = 1.333432
+d = -50.59
+print(type(a))
+print(type(s))
+print(type(d))
+#complex Los números complejos se escriben con una "j" como parte imaginaria:
+q = 7+7j
+w = 88j
+e = -5j
+print(type(q))
+print(type(w))
+print(type(e))
