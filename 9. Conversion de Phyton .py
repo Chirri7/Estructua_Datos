b2e2e2dcf02d3c8030934482ea0e51407c3e3c03
Chirri7 <varelaj565@gmail.com>
2024-09-19 14:48:07 -0500

    Add files via upload

---
+++ 9. Conversion de Phyton .py
@@ -0,0 +1,14 @@
+#Podemos especificar el tipo de variable desde el inicio utilizando las siguientes funciones 
+x = int(77)   # x tendra el valor de 77 y sera si o si de tipo entero
+y = int(48.8) # y tendra el valor de 48.8 y sera si o si un numero flotante 
+z = int("2") # z tendra si o si una caracteristica
+#a continuacion se vera un ejemplo de numeros flotantes 
+c = float(1)      
+v = float(2.8)   
+b = float("3")   
+n = float("4.2") 
+# como estoy obligando que sean numeros flotantes, apareceran valor flotantes 
+print(c)
+print(v)
+print(b)
+print(n)
