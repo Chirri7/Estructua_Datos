3ac7e55cd88c4a7927bfb7629ea11027dffe59ca
Chirri7 <varelaj565@gmail.com>
2024-09-23 01:46:44 -0500

    Add files via upload

---
+++ 13.Concatenacion de Cadenas.py
@@ -0,0 +1,15 @@
+#La concatenacion funciona para unir los elementos guardados de dos variables. 
+#Si deseamos concatenar dos cadenas utilizamos lo siguiente: 
+a = "Colombia es "
+b = "lo mejor. "
+c = a+b;
+print(c) #Aqui estamos mostrando las dos cadenas unidas
+a = "Colombia es "
+b = "lo mejor. "
+c = a+" "+b;# de esta manera podemos poner un espacio entre las dos 
+#se pueden unir muchas cadenas a la vez 
+s = "el mundo es pequeño"
+d = "la Uis es una universidad"
+f = "The walking dead"
+t = s+d+f;
+print(t) #Mostramos todas las 3 cadenas concatenadas
